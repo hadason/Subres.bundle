8f360985ecffcdd8594a081375237b7bdf2a191e
U-Hadas-Laptop\Hadas <Hadas@Hadas-Laptop.(none)>
2013-05-05 01:11:14 -0400

    Fixed number reversing issue, added exception information

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,16 +1,23 @@
+# -*- coding: iso-8859-8 -*-
+
 #Subres - Converts hebrew subtitles from ANSI to Unicode and provides RTL support.
 
-import os, string, hashlib, base64, re, plistlib, unicodedata, codecs
+import os, string, hashlib, base64, re, plistlib, unicodedata, codecs, sys
+
+class LineStatus:
+    LINE_NUMBER = 1
+    TIME = 2
+    SUBTITLES = 3
 
 def Start():
     Log('Subres - start() called')
 
-def reverseHeb(match):
+def reverseGroup(match, groupName):
         line = ''
         for group in match.groups():
             if (group is None):
                 continue
-            if (group == match.group('subs')):
+            if (group == match.group(groupName)):
                 line += group[::-1]
             else:
                 line += group
@@ -18,19 +25,26 @@ def reverseHeb(match):
 
 def writeHebSubs(inputFile):
     input = os.fdopen(os.open(inputFile, os.O_RDONLY))
-    lineRegex = re.compile(r'(^[0-9].*[0-9]*$)', re.MULTILINE)
     lines = input.readlines()
+    state = LineStatus.LINE_NUMBER
     result = ''
     for line in lines:
-        if (lineRegex.match(line)):
+        if (state == LineStatus.LINE_NUMBER):
             result += line
-        else:
-            fix = re.sub('(<[a-zA-Z]>)*([\.\,\"\-:\?\'!\(\)a-zA-Z]*)(?P<subs>.*?)([\.\,\"\-:\?\'!\(\)a-zA-Z]*)(</[a-zA-Z]>)*$', lambda match: reverseHeb(match), line)
-            re.sub('([0-9]+)', lambda match: match.group(0)[::-1], fix)
-            result += fix
+            state = LineStatus.TIME
+            continue
+        if (state == LineStatus.TIME):
+            result += line
+            state = LineStatus.SUBTITLES
+            continue
+        if (line == '\n'):
+            state = LineStatus.LINE_NUMBER
+        line = re.sub('(<[a-zA-Z]>)*([\.\,\"\-:\?\'!\(\)a-zA-Z]*)(?P<subs>.*[à-ú]+.*?)([\.\,\"\-:\?\'!\(\)a-zA-Z]*)(</[a-zA-Z]>)*$', lambda match: reverseGroup(match, 'subs'), line)
+        line = re.sub(r'([à-ú]+.*)(?P<nums>[0-9]+)', lambda match: reverseGroup(match, 'nums'), line)
+        line = re.sub(r'(?P<nums>[0-9]+)(.*[à-ú])+', lambda match: reverseGroup(match, 'nums'), line)
+        result += line
     input.close()
     return codecs.BOM_UTF8 + result.decode('iso-8859-8').encode('utf-8')
-    #return result
     
 
 class SubresAgentMovies(Agent.Movies):
@@ -59,9 +73,10 @@ class SubresAgentMovies(Agent.Movies):
             else:
                 Log('Subtitle file doesnt exist')
                 Log(dir(Proxy))
-        except:
-            Log('Error')
+        except Exception as e:
+            Log('Error: ' + e)
 
+            
 class SubresSubtitlesAgentTV(Agent.TV_Shows):
   name = 'Subres_TV'
   languages = [Locale.Language.English]
